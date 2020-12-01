abe2b73812df1834be07c27430d1971bc8b27a94
prollyhollywood <72751673+prollyhollywood@users.noreply.github.com>
2020-11-30 21:27:05 -0500

    Update main.ts, main.py

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 while True:
-    print("temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT))
-    if input.temperature(TemperatureUnit.FAHRENHEIT) > 100:
-        light.set_all (light.rgb(0, 255, 0))
-    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 100:
-        light.set_all (light.rgb(255, 0, 0))
-    elif input.temperature(TemperatureUnit.FAHRENHEIT) == 80:
-        light.set_all (light.rgb (255, 255, 0))
+    print("Temperature: " + input.temperature(TemperatureUnit.FAHRENHEIT))
+    if input.temperature(TemperatureUnit.FAHRENHEIT) >= 212:
+        light.set_all(light.rgb(0,255,0))
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) == 200: 
+        light.set_all(light.rgb(255,255,0))
+    elif input.temperature(TemperatureUnit.FAHRENHEIT) < 212: 
+        light.set_all(light.rgb(255,0,0))
